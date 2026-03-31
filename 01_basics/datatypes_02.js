280bd8f2dd4067cdc2fb555dd557e08a5d576c6c
rishabhsingh2108 <sunnyrajawat990@gmail.com>
2026-03-31 18:12:08 +0000

    1,2,3 complete

---
+++ 01_basics/datatypes_02.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") we are using nodejs, not browser
+
+let name = "rishabh" //string
+let age = 18 // number
+let isloggedIn = false //boolean
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// underfined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined ); //undefined
+console.log(typeof null ); // object
---
+++ 01_basics/variable.js
@@ -0,0 +1,15 @@
+const accountId = 13344
+let accountEmail = "rishabh@gmail.com"
+var accountPassword = "12234"
+accountCity = "Gwalior"
+let accountState;
+
+// accountId = 3 //
+
+accountEmail = "gmail.com"
+accountPassword = "2222444"
+accountCity = "bengaluru"
+
+console.log(accountId);
+console.table([ accountEmail , accountId, accountPassword, accountCity, accountState ])
+  
